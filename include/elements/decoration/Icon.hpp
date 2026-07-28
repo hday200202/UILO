e07eb64a310c4827e19f22ffe32d2f8fabc75e00
harrison <harrison.day@omnisp.com>
2026-07-28 16:54:26 -0500

    Add new SVG icons and create Icon class structure
    - Added various SVG icons including trending-up, triangle, truck, tv, twitch, twitter, type, umbrella, underline, unlock, upload-cloud, upload, user-check, user-minus, user-plus, user-x, user, users, video-off, video, voicemail, volume-1, volume-2, volume-x, volume, watch, wifi-off, wifi, wind, x-circle, x-octagon, x-square, x, youtube, zap-off, zap, zoom-in, and zoom-out.
    - Created Icon.hpp header file with basic structure for Icon and IconOptions classes.

---
+++ include/elements/decoration/Icon.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "../Element.hpp"
+
+
+namespace uilo {
+
+
+class IconOptions {
+public:
+
+private:
+
+};
+
+
+class Icon : public Element {
+public:
+
+private:
+
+};
+
+
+}
